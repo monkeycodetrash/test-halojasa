a0f9154ffc73ddf4ef0fc4fa6a90c3acdd041c05
monkeycodetrash <herry.yonatan@gmail.com>
2019-12-04 12:34:09 +0700

    2nd commit

--- src/components/Contents/Additional/Additional.js
+++ src/components/Contents/Additional/Additional.js
@@ -4,24 +4,26 @@ import cssAdditional from './Additional.css';
 const additional = () => {
     return (
         <div className={cssAdditional.Additional}>
-            <div className={cssAdditional.flex}>
-                <div>
-                    <p>Check out our Blog!</p>
-                    <span><a href="#">How to drink water</a></span>
-                    <span><a href="#">Our product is the best</a></span>
-                    <span><a href="#">How to start a kickstarter</a></span>
-                </div>
-                <div>
-                    <p>Company Event</p>
-                    <span><a href="#">Event Schedule</a></span>
-                    <span><a href="#">Product Launches</a></span>
-                    <span><a href="#">Announcement</a></span>
-                </div>
-                <div>
-                    <p>Join Our Newsletter</p>
+            <div className={cssAdditional.Wrap}>
+                <div className={cssAdditional.Flex}>
                     <div>
-                        <input type="email" />
-                        <button>Join</button>
+                        <p>Check out our Blog!</p>
+                        <span><a href="#">How to drink water</a></span>
+                        <span><a href="#">Our product is the best</a></span>
+                        <span><a href="#">How to start a kickstarter</a></span>
+                    </div>
+                    <div>
+                        <p>Company Event</p>
+                        <span><a href="#">Event Schedule</a></span>
+                        <span><a href="#">Product Launches</a></span>
+                        <span><a href="#">Announcement</a></span>
+                    </div>
+                    <div>
+                        <p>Join Our Newsletter</p>
+                        <div>
+                            <input type="email" />
+                            <button>JOIN</button>
+                        </div>
                     </div>
                 </div>
             </div>
